f45f257f07a748c590df4644376ee76c2e3cb4fe
georgekravtsov <gorgkravtsov@gmail.com>
2022-01-24 17:05:20 +0300

    The code has been reorganized in a more idiomatic way, the discount calculation functionality has been added

--- main.go
+++ main.go
@@ -3,8 +3,13 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"strings"
 	"time"
 )
+
+func main() {
+	checkPrint(10000, 10, 100, 10, 100)
+}
 
 func cardNumberGeneration(maxNumber int) int {
 	rand.Seed(time.Now().UnixNano())
@@ -32,14 +37,16 @@ func priceGeneration(maxOneItemPrice int) float64 {
 }
 
 func checkPrint(maxCardNumber, maxNumberOfItems, maxItemId, oneItemMaxQuantity, oneItemMaxPrice int) {
-	fmt.Println("_______________________________________________")
-	fmt.Printf("|Discount card: %d %27s\n", cardNumberGeneration(maxCardNumber), "|")
+	cardNumber := cardNumberGeneration(maxCardNumber)
+	discount := discountGeneration(cardNumber)
+
 	fmt.Println("_______________________________________________")
 	fmt.Printf("|%2s | %10s | %s| %s| %s|\n", "№", "ItemId", "Quantity", "Price", "Total Price")
 	fmt.Println("_______________________________________________")
 	reverseCounter := numberOfItemsInCheck(maxNumberOfItems)
 	itemCounter := 1
 	var total float64
+	var toBePaid float64
 	for {
 		id := itemIdGeneration(maxItemId)
 		quantity := itemQuantityGeneration(oneItemMaxQuantity)
@@ -47,6 +54,7 @@ func checkPrint(maxCardNumber, maxNumberOfItems, maxItemId, oneItemMaxQuantity, 
 		oneItemTotal := float64(quantity) * oneItemPrice
 		fmt.Printf("|%2d | %10d | %8d| %5.2f| %11.2f|\n", itemCounter, id, quantity, oneItemPrice, oneItemTotal)
 		total += oneItemTotal
+		toBePaid = total - (total * discount)
 		if reverseCounter <= 0 {
 			break
 		}
@@ -58,8 +66,27 @@ func checkPrint(maxCardNumber, maxNumberOfItems, maxItemId, oneItemMaxQuantity, 
 	fmt.Println("_______________________________________________")
 	fmt.Printf("|Total: %.2f %32s\n", total, "|")
 	fmt.Println("_______________________________________________")
+	fmt.Printf("|Discount card: %d %27s\n", cardNumber, "|")
+	fmt.Println("_______________________________________________")
+	fmt.Printf("|Discount: %.0f%s %34s\n", discount*100, "%", "|")
+	fmt.Println("_______________________________________________")
+	fmt.Printf("|To be paid: %.2f; Saved: %.2f %14s\n", toBePaid, total-toBePaid, "|")
+	fmt.Println("_______________________________________________")
 }
 
-func main() {
-	checkPrint(10000, 10, 100, 10, 100)
+func discountGeneration(cardNumber int) float64 {
+	stringCardNumber := fmt.Sprintf("%d", cardNumber)
+	numberOfSevens := strings.Count(stringCardNumber, "7")
+	switch numberOfSevens {
+	case 1:
+		return 0.07
+	case 2:
+		return 0.17
+	case 3:
+		return 0.37
+	case 4:
+		return 0.7
+	default:
+		return 0.03
+	}
 }
