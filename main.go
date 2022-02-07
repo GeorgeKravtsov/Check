260de362a006b4e918cc14d1f955d415250b213e
georgekravtsov <gorgkravtsov@gmail.com>
2022-02-07 20:52:41 +0300

    preparing the implementation of the web interface

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
 
 //	maxCardNumber := getUserInput("Enter maximum discount card number (10000 for example) ",
@@ -14,12 +19,24 @@ func main() {
 //	promotionDiscount := getUserInput("Enter promotion discount (10 for exmple): ",
 //						"Promotion discount is:")
 
-	recAut := newReceiptAuto(10000, 10, 100, 10, 100, 10) //maxCardNumber, maxNumberOfItems,
+//	recAut := newReceiptAuto(10000, 10, 100, 10, 100, 10) //maxCardNumber, maxNumberOfItems,
 				//maxItemId, oneItemMaxQuantity, oneItemMaxPrice, promotionDiscount
-	printReceiptAuto(recAut)
+//	printReceiptAuto(recAut)
 
-	recMan := newReceipt(100, 12,100,10,100,10.0)
-	printReceipt(recMan)
+//	recMan := newReceipt(100, 12,100,10,100,10.0)
+//	printReceipt(recMan)
+
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprint(writer, "Welcome Page")})
+
+	http.HandleFunc("/instruments", func(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprint(writer, "Instruments Pge")})
+
+	http.HandleFunc("/receipt", func(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprint(writer, "Receipt Page")})
+
+	fmt.Println("Server is listening...")
+	http.ListenAndServe("localhost:8181", nil)
 }
 
 
